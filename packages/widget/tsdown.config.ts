3038bbd4f83fd1a973b722243105e685c18ccaec
CreeJee <000as00060@gmail.com>
2026-07-05 23:30:30 +0900

    refactor: move widget source into packages/widget (@agentic-prd/widget)

--- packages/widget/tsdown.config.ts
+++ packages/widget/tsdown.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "tsdown";
 import packageJSON from "./package.json" with { type: "json" };
 
-// peerDependencies(react/react-dom)는 호스트가 제공하므로 번들에 포함하지 않는다.
+/**
+ * peerDependencies(react/react-dom)는 호스트가 제공하므로 번들에 포함하지 않는다.
+ */
 const neverBundle = Object.keys(packageJSON.peerDependencies);
 
 export default defineConfig({
